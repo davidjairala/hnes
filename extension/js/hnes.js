d7ef75116951752135fac8f0c0b4664d1c108342
David Jairala <davidjairala@gmail.com>
2012-11-16 21:30:45 -0800

    fix arrow stuff in one fell swoop

--- extension/js/hnes.js
+++ extension/js/hnes.js
@@ -1,41 +1,44 @@
 jQuery(function() {
   var hnesExtension = function(options) {
-    this.options = options || {};
+    this.options      = options || {};
+    this.pattern_down = /^down_/;
+    this.pattern_up   = /^up_/;
 
     this.replace_arrows();
+    this.fix_arrow_height();
   };
 
   hnesExtension.prototype = {
-    // Replace arrows with html entities
     replace_arrows : function() {
-      var pattern_down = /^down_/;
-      var pattern_up = /^up_/;
-      var pattern_match_up;
-      var pattern_match_down;
       var id;
       var up_arrow = '&uarr;';
       var down_arrow = '&darr;';
       var that = this;
 
-
       $('a').each(function(idx, val) {
         id = $(this).attr('id');
         
         if(id !== undefined) {
-          pattern_match_up    = pattern_up.test(id);
-          pattern_match_down  = pattern_down.test(id);
-
-          if(pattern_match_up || pattern_match_down) {
-            if(pattern_match_up) {
-              $(this).css('display', 'block');
-              $(this).css('height', '50px');
-              $(this).css('padding-top', '10px');
-
-              $(this).html(up_arrow);
-            } else if(pattern_match_down) {
-              $(this).html(down_arrow);
-            }
+          if(that.pattern_up.test(id)) {
+            $(this).html(up_arrow);
+          } else if(that.pattern_down.test(id)) {
+            $(this).html(down_arrow);
           }
+        }
+      });
+    },
+
+    fix_arrow_height : function() {
+      var id;
+      var that = this;
+
+      $('body table table table a').each(function(idx, val) {
+        id = $(this).attr('id');
+        
+        if(id !== undefined && that.pattern_up.test(id)) {
+          $(this).css('display', 'block');
+          $(this).css('height', '50px');
+          $(this).css('padding-top', '10px');
         }
       });
     }
